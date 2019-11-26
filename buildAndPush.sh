939d328af9b22a237edea4e3f7d67f5d05cf98bd
Dirk <dstrauss@ds-2.de>
2019-11-26 10:19:09 +0100

    - current snapshot

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -4,14 +4,14 @@ function buildImage(){
     local dkrPath=$1
     local tagName=$2
     local tagVersion=${3:-latest}
-    echo "Building image from $dkrPath with tagName $tagName and version $tagVersion.."
-    docker build --rm -f "$dkrPath/Dockerfile" -t "$tagName:$tagVersion" "$dkrPath"
+    echo "Building image from $dkrPath with tagName $tagName and tag $tagVersion.."
+    docker build --pull --rm -f "$dkrPath/Dockerfile" -t "$tagName:$tagVersion" "$dkrPath"
 }
 
 function pushImage(){
     local tagName=$1
     local tagVersion=${2:-latest}
-    echo "Pushing image $tagName with version $tagVersion to Docker Hub.."
+    echo "Pushing image $tagName with tag $tagVersion to Docker Hub.."
     docker push "$tagName:$tagVersion"
 }
 
@@ -23,6 +23,11 @@ function runTempImage(){
     echo "Run: $imgExec"
 }
 
-buildImage "cicd/jkslave" "dstrauss/jk-jnlp-slave" "3.29-1"
+echo "Building some images with tags.."
+# buildImage "cicd/jkslave" "dstrauss/jk-jnlp-slave" "3.29-1"
+buildImage "cicd/jmeter" "dstrauss/jmeter-runner" "5.1.1"
+
+echo "Pushing some of these now to Docker.."
 # pushImage "dstrauss/jk-jnlp-slave" "3.29-1"
-runTempImage "dstrauss/jk-jnlp-slave" "3.29-1"
+pushImage "dstrauss/jmeter-runnter" "5.1.1"
+# runTempImage "dstrauss/jk-jnlp-slave" "3.29-1"
