4069304e663c8e39728aba6d6decadfa79b7a0e0
Dirk <dstrauss@ds-2.de>
2020-04-20 18:37:53 +0200

    - csv export

--- cicd/jmeter/runTest.sh
+++ cicd/jmeter/runTest.sh
@@ -5,7 +5,7 @@ JM_LOGS=/jmeter-logs
 JM_SRC=/jmeter-src
 # JM_PROPS must be given by ENV
 
-MYCMD="jmeter -n -t ${JM_SRC}/$JMETER_FILENAME -l ${JM_LOGS}/jmeter-samples.log -j ${JM_LOGS}/jmeter.log -e -o ${JM_RESULTS} ${JM_PROPS}"
+MYCMD="jmeter -n -t ${JM_SRC}/$JMETER_FILENAME -l ${JM_RESULTS}/sample-results.jtl -j ${JM_LOGS}/jmeter.log -e -o ${JM_RESULTS} ${JM_PROPS}"
 
 echo "Will run now: $MYCMD"
 
