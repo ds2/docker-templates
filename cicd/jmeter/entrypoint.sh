bbf27f813e9a8eac5718f043902f89b3df8a5d02
Dirk <dstrauss@ds-2.de>
2020-04-20 16:54:16 +0200

    - use jmeter 5.2.1

--- cicd/jmeter/entrypoint.sh
+++ cicd/jmeter/entrypoint.sh
@@ -6,9 +6,11 @@ export PATH=$PATH:/jmeter/bin
 
 if [ ! -w /jmeter-logs ]; then
     echo "(!) Cannot write into logs dir!! There may be issues in the future!!"
+    exit 1
 fi
 if [ ! -w /jmeter-results ]; then
     echo "(!) Cannot write into results dir!! There may be issues in the future!!"
+    exit 2
 fi
 
 eval "$@"
