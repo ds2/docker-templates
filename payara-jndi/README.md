cc9322b29184b27cd40394f03b07f316cc0a6ff0
Dirk Strauss <dstrauss@ds-2.de>
2021-03-08 18:29:38 +0100

    DT-1: seems to work

--- payara-jndi/README.md
+++ payara-jndi/README.md
@@ -8,4 +8,4 @@ To autoconfigure a payara server on boot to directly deploy JNDI WARs.
 
 ## How to test
 
-    docker run -it --rm -p 4848:4848 -p 8080:8080 -e JNDI_1_ID=testPool -e JNDI_1_DSCLASS=org.postgresql.ds.PGConnectionPoolDataSource -e JNDI_1_JNDI=jdbc/test1 payarajndi:latest
+    docker run -it --rm -p 4848:4848 -p 8080:8080 -e JNDI_1_ID=testPool -e JNDI_1_DSCLASS=org.postgresql.ds.PGConnectionPoolDataSource -e JNDI_1_JNDI=jdbc/test1 -e JNDI_1_USER=myusr -e JNDI_1_PW=mypw -e JNDI_1_DBNAME=mydb payarajndi:latest
--- payara-jndi/registerJndis.sh
+++ payara-jndi/registerJndis.sh
@@ -17,6 +17,16 @@ function fn() {
     echo ${!1}
 }
 
+function join_by() {
+    # local IFS=":"
+    # shift
+    # echo "$*"
+    local separator=':'
+    local first="$1"
+    shift
+    printf "%s" "$first" "${@/#/$separator}"
+}
+
 function printAsAdm() {
     local id=$1
     local headerName="\$JNDI_${id}"
@@ -32,12 +42,16 @@ function printAsAdm() {
     local JNDI_RESIZE=$(fn JNDI_${id}_RESIZE)
     local JNDI_STCACHE=$(fn JNDI_${id}_STCACHE)
     local JNDI_LEAKT0=$(fn JNDI_${id}_LEAKT0)
+    local JNDI_STLEAKT0=$(fn JNDI_${id}_STLEAKT0)
     local JNDI_USER=$(fn JNDI_${id}_USER)
     local JNDI_PW=$(fn JNDI_${id}_PW)
     local JNDI_PORT=$(fn JNDI_${id}_PORT)
     local JNDI_HOSTNAME=$(fn JNDI_${id}_HOSTNAME)
     local JNDI_DBNAME=$(fn JNDI_${id}_DBNAME)
     local JNDI_DSNAME=$(fn JNDI_${id}_DSNAME)
+    local JNDI_URL=$(fn JNDI_${id}_URL)
+    local JNDI_MAXCONNUSE=$(fn JNDI_${id}_MAXCONNUSE)
+    local JNDI_SHOWSQL=$(fn JNDI_${id}_SHOWSQL)
     if [ -z "$JNDI_ID" ]; then
         echo ""
     else
@@ -45,12 +59,17 @@ function printAsAdm() {
         # asadmin set domain.resources.jdbc-connection-pool.__TimerPool.slow-query-threshold-in-seconds=50
         CMD="$CMD --validateatmostonceperiod=300"
         CMD="$CMD --poolresize=${JNDI_RESIZE:-2} --steadypoolsize=${JNDI_MINSIZE:-0} --maxpoolsize=${JNDI_MAXSIZE:-5}"
-        CMD="$CMD --statementtimeout=${JNDI_STATT0:-200} --statementleaktimeout=240 --statementleakreclaim=true --statementcachesize=${JNDI_STCACHE:-50}"
+        CMD="$CMD --statementtimeout=${JNDI_STATT0:-0} --statementcachesize=${JNDI_STCACHE:-0}"
         # CMD="$CMD --statementleaktimeout=${JNDI_STATT0:-5}"
-        CMD="$CMD --leaktimeout=${JNDI_LEAKT0:-5} --leakreclaim=true"
+        if [ -n "$JNDI_LEAKT0" ]; then
+            CMD="$CMD --leaktimeout=${JNDI_LEAKT0:-0} --leakreclaim=true"
+        fi
+        if [ -n "$JNDI_STLEAKT0" ]; then
+            CMD="$CMD --statementleaktimeout=${JNDI_STLEAKT0} --statementleakreclaim=true"
+        fi
         CMD="$CMD --creationretryattempts=3 --creationretryinterval=9"
-        CMD="$CMD --logjdbccalls=true"
-        CMD="$CMD --maxconnectionusagecount=100"
+        CMD="$CMD --logjdbccalls=${JNDI_SHOWSQL:-false}"
+        CMD="$CMD --maxconnectionusagecount=${JNDI_MAXCONNUSE:-0}"
         #  set resources.jdbc-connection-pool.test-pool.statement-leak-timeout-in-seconds=5
         #asadmin> set resources.jdbc-connection-pool.test-pool.connection-leak-timeout-in-seconds=5
         if [ ! -z "$JNDI_DSCLASS" ]; then
@@ -59,11 +78,37 @@ function printAsAdm() {
             echo "You must tell what DS class can be used! Set ENV var ${headerName}_DSCLASS!"
             exit 1
         fi
-        if [ ! -z "$JNDI_PROPS"]; then
-            CMD="$CMD --property \"${JNDI_PROPS}\""
-        fi
         if [ ! -z "$JNDI_DESCR" ]; then
             CMD="$CMD --description=\"${JNDI_DESCR}\""
+        fi
+        PROPS=""
+        if [ ! -z "$JNDI_PROPS" ]; then
+            PROPS="$JNDI_PROPS"
+        else
+            PROPARRAY=()
+            if [ -n "$JNDI_USER" ]; then
+                PROPARRAY+=("user=$JNDI_USER")
+            fi
+            if [ -n "$JNDI_PW" ]; then
+                PROPARRAY+=("password=$JNDI_PW")
+            fi
+            if [ -n "$JNDI_PORT" ]; then
+                PROPARRAY+=("portNumber=$JNDI_PORT")
+            fi
+            if [ -n "$JNDI_HOSTNAME" ]; then
+                PROPARRAY+=("serverName=$JNDI_HOSTNAME")
+            fi
+            if [ -n "$JNDI_DBNAME" ]; then
+                PROPARRAY+=("databaseName=$JNDI_DBNAME")
+            fi
+            if [ -n "$JNDI_URL" ]; then
+                PROPARRAY+=("url=$JNDI_URL")
+            fi
+            # echo "# Props are ${PROPARRAY[@]} -> ${PROPARRAY}"
+            PROPS=$(join_by "${PROPARRAY[@]}")
+        fi
+        if [ ! -z "$PROPS" ]; then
+            CMD="$CMD --property \"${PROPS}\""
         fi
         CMD="$CMD $JNDI_ID"
         echo "$CMD"
@@ -76,8 +121,11 @@ for name in {1..5}; do
         JNDI_SLOWQUERY=$(fn JNDI_${name}_SLOWQUERY)
         JNDI_JNDI=$(fn JNDI_${name}_JNDI)
         JNDI_DESCR=$(fn JNDI_${id}_DESCR)
+        JNDI_STLEAKT0=$(fn JNDI_${id}_STLEAKT0)
         echo $(printAsAdm $name) >>$POSTBOOTCMDS
         echo "set domain.resources.jdbc-connection-pool.${JNDI_ID}.slow-query-threshold-in-seconds=${JNDI_SLOWQUERY:-10}" >>$POSTBOOTCMDS
+        echo "set domain.resources.jdbc-connection-pool.${JNDI_ID}.statement-leak-timeout-in-seconds=${JNDI_STLEAKT0:-5}" >>$POSTBOOTCMDS
+        echo "set domain.resources.jdbc-connection-pool.${JNDI_ID}.statement-leak-reclaim=true" >>$POSTBOOTCMDS
         if [ ! -z "$JNDI_JNDI" ]; then
             echo "create-jdbc-resource --connectionpoolid ${JNDI_ID} --description=\"${JNDI_DESCR:-my sample dummy jndi}\" $JNDI_JNDI" >>$POSTBOOTCMDS
         fi
@@ -86,4 +134,4 @@ done
 
 cat $POSTBOOTCMDS
 
-#exit 1
+# exit 1
