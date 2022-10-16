61d3328792315f9dc1593702f639691f44188a34
Lexxy <472167-dstrauss@users.noreply.gitlab.com>
2022-10-16 21:37:26 +0200

    🚧 set some default values for the image

--- enterpriselinux-rust/entrypoint.sh
+++ enterpriselinux-rust/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "My path is actually: ${PATH}"
 
 export CARGO_HOME=${CARGO_HOME:-/home/rusty/.cargo}
+export SRC_DIR=${SRC_DIR:-/work}
+export CARGO_BUILD_TARGET=${CARGO_BUILD_TARGET:-'x86_64-unknown-linux-gnu'}
+export CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-/tmp/rust-stuff}
 
 if [ "${CARGO_HOME}" != "/home/rusty/.cargo" ]; then
     echo "Copying existing cargo bins from ~/.cargo/bin/ to new CARGO_HOME at $CARGO_HOME/bin/"
