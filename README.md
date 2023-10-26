4b369f3500c9b1f294c02b97e39c5a249d6b831e
Lexxy <472167-dstrauss@users.noreply.gitlab.com>
2023-10-26 11:57:51 +0200

    🔨 add tagging and buildargs

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Use your quay.io Username and the "Docker CLI" encrypted password to login.
 After that you can build your image, and then:
 
     ./buildAndPush.sh -d MYDIRECTORY -r quay.io/ds2/myimage -p
+
+Example:
+
+    ./buildAndPush.sh -d rust-debian -r quay.io/ds2/rust-debian -b "linuxDistro=debian  distroVersion=bookworm" -t "bookworm bw-$(git rev-parse --short=8 HEAD)"
+    ./buildAndPush.sh -d rust-debian -r quay.io/ds2/rust-debian -b "linuxDistro=debian  distroVersion=bullseye" -t "bullseye be-$(git rev-parse --short=8 HEAD)"
+    ./buildAndPush.sh -d rust-debian -r quay.io/ds2/rust-debian -b "linuxDistro=ubuntu  distroVersion=jammy" -t "jammy jm-$(git rev-parse --short=8 HEAD)"
--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
-GITHASH=$(git rev-parse --short=10 HEAD)
+# GITHASH=$(git rev-parse --short=10 HEAD)
 
 PUSH=0
 DIRECTORY=""
 REPO="delme"
+TAGS=""
+BUILD_ARGS=""
 
-while getopts ':pd:b:r:' OPTION; do
+while getopts ':pd:b:r:t:b:' OPTION; do
     case "$OPTION" in
     p) PUSH=1 ;;
     d) DIRECTORY="$OPTARG" ;;
-    r) REPO="$OPTARG";;
-    *) echo "Unbekannter Parameter" ;;
+    r) REPO="$OPTARG" ;;
+    t) TAGS="$OPTARG" ;;
+    b) BUILD_ARGS="$OPTARG" ;;
     \?) echo "Invalid param: $OPTARG" ;;
+    *)
+        echo "Unbekannter Parameter"
+        echo "Usage:"
+        echo "$0"
+        echo "  -d DIRECTORY = defines the directory to look for the Containerfile"
+        echo "  -r REPO = sets the registry and repository to use; i.e. quay.io/org/image"
+        echo "  -p = push to REPO"
+        echo "  -b ""a=1 b=2"" = defines some build args"
+        echo "As tags, we use the git short checksum, and LATEST."
+        ;;
     esac
 done
 
@@ -27,19 +40,28 @@ fi
 
 function uploadImage() {
     local repo="$1"
-    echo "Retagging $repo from latest to $GITHASH"
-    podman tag ${repo}:latest ${repo}:${GITHASH} || exit 1
-    echo "Pushing image with hash first.."
-    podman push ${repo}:${GITHASH} || exit 2
-    echo "And now the latest tag"
-    podman push ${repo}:latest || exit 3
+    for t in $TAGS; do
+        echo "Pushing image with tag $t .."
+        #podman tag ${repo}:latest ${repo}:${t} || exit 1
+        podman push ${repo}:${t} || exit 2
+    done
 }
 
-function buildImage(){
+function buildImage() {
     local dir=$1
     local repo=$2
     echo "Building image $repo.."
-    podman build -t "${repo}:latest" $dir || exit 1
+    CMD="podman build -t ""${repo}:latest"" "
+    for b in $BUILD_ARGS; do
+        CMD+=" --build-arg ""$b"""
+    done
+    CMD+=" $dir"
+    echo "  will use CMD: $CMD"
+    eval $CMD || exit 1
+    for t in $TAGS; do
+        echo "retagging latest as $t"
+        podman tag $repo:latest $repo:$t || exit 3
+    done
     echo "You may run the image now via:"
     echo "  podman run -it --rm ${repo}:latest"
 }
